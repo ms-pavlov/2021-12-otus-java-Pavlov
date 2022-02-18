70fee98283d12ce1ba4bd0a2476e81383166573a
ms-pavlov <kilai.ru@yandex.ru>
2022-02-18 18:55:34 +0900

    Домашнее задание
    Эмулятор банкомата

--- hw06-gradle/src/main/java/ru/otus/hw06/interfaces/Banknotes.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw06.interfaces;
-
-import ru.otus.hw06.exceptions.BanknotesNominalExceptions;
-
-public interface Banknotes {
-    double getNominal () throws BanknotesNominalExceptions;
-}
